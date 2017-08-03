42d8b55452cd0105c061be532921d8b21940297c
Joshuavarghese527 <joshuavarghese527@gmail.com>
2017-08-03 01:36:29 -0400

    User Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,14 @@ import { TweetListComponent } from './tweet-list/tweet-list.component';
 import { TweetComponent } from './tweet/tweet.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
 import { HashtagComponent } from './hashtag/hashtag.component';
+import { UserComponent } from './user/user.component';
+import { UserService } from './user.service';
 
 const appRoutes: Routes = [
   { path: '', component: MainPageComponent },
   { path: 'about', component: AboutPageComponent },
-  { path: 'hashtag/:hashtag', component: HashtagComponent }
+  { path: 'hashtag/:hashtag', component: HashtagComponent },
+  { path: 'user/:username', component: UserComponent }
 ];
 
 @NgModule({
@@ -28,13 +31,14 @@ const appRoutes: Routes = [
     TweetListComponent,
     TweetComponent,
     SearchbarComponent,
-    HashtagComponent
+    HashtagComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     HttpModule, RouterModule.forRoot(appRoutes)
   ],
-   providers: [ TweetsService ],
+   providers: [ TweetsService, UserService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class UserService {
+
+  constructor(private http: Http) { }
+
+  getUser(username: string) {
+    const url = `https://twitter-api-jayvee527.herokuapp.com/users/${username}`;
+    return this.http.get(url)
+    .map(this.extractData)
+   .catch(this.handleError);
+  }
+
+  extractData(res: Response) {
+    return res.json();
+  }
+
+  handleError(error: Response | any) {
+    console.log(error);
+    return Observable.throw(error);
+  }
+
+} 
+ 
+ 
+ 
+ 
+ 
+ 
